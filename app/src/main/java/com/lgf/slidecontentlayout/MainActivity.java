83f5a149acb2d9260fccb24475c8b5e14d7886ff
ligf <ligf@careland.com.cn>
2018-03-19 20:03:45 +0800

    1.实现RecyclerView的嵌套布局的拖动 2.设置布局的位置三种模式，松手后返回到其中一种布局

--- app/src/main/java/com/lgf/slidecontentlayout/MainActivity.java
+++ app/src/main/java/com/lgf/slidecontentlayout/MainActivity.java
@@ -1,35 +1,89 @@
 package com.lgf.slidecontentlayout;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private RelativeLayout mDataContainerLayout;
     private SlideContentLayout mSlideContentLayout;
+    private RecyclerView mDataRecyclerView;
+    private List<String> mDatas = new ArrayList<>();
+    private DataAdapter mDataAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
         initViews();
     }
 
+    private void initData(){
+        mDatas.clear();
+        for (int i = 0; i < 20; i ++){
+            mDatas.add(i + "");
+        }
+    }
+
     private void initViews(){
-//        mDataContainerLayout = (RelativeLayout) findViewById(R.id.rl_data_content_layout);
-//        Button button = new Button(this);
-//
-//        mSlideContentLayout = new SlideContentLayout(this);
-//        mSlideContentLayout.setBackgroundColor(Color.GREEN);
-//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-////        layoutParams.addRule(Gravity.BOTTOM);
-//        layoutParams.height = 100;
-//
-//        mDataContainerLayout.addView(mSlideContentLayout, layoutParams);
+        mDataContainerLayout = (RelativeLayout) findViewById(R.id.rl_data_content_layout);
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View dataLayout = layoutInflater.inflate(R.layout.slide_content_layout, null);
+        mDataRecyclerView = (RecyclerView) dataLayout.findViewById(R.id.recyclerview_data_list);
+        mDataAdapter = new DataAdapter();
+        mDataRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mDataRecyclerView.setAdapter(mDataAdapter);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        dataLayout.setY(400);
+        mDataContainerLayout.addView(dataLayout);
+
+    }
+
+    private class DataAdapter extends RecyclerView.Adapter<DataAdapter.DataViewHolder>{
+
+        @Override
+        public DataAdapter.DataViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.data_list_item_layout, parent, false);
+            DataViewHolder dataViewHolder = new DataViewHolder(layout);
+            return dataViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(DataAdapter.DataViewHolder holder, int position) {
+            Log.i(TAG, "onBindViewHolder position:" + position);
+            Log.i(TAG, "onBindViewHolder getItemCount:" + getItemCount());
+
+            holder.contentTV.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas == null ? 0 : mDatas.size();
+        }
+
+        class DataViewHolder extends RecyclerView.ViewHolder{
+
+            TextView contentTV;
+
+            public DataViewHolder(View itemView) {
+                super(itemView);
+                contentTV = (TextView) itemView.findViewById(R.id.tv_data_list_item_content);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/lgf/slidecontentlayout/SlideContentLayout.java
+++ app/src/main/java/com/lgf/slidecontentlayout/SlideContentLayout.java
@@ -1,24 +1,44 @@
 package com.lgf.slidecontentlayout;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 /**
  * Created by garment on 2018/3/18.
  */
 
+/**
+ * 仿百度地图的地址列表的上拉，下拉拖动效果
+ * 需指定上拉的最大高度，还有下拉的最小高度
+ * 原理：通过设置View的位置，实现View的移动(如果使用改变View的大小进行移动动画，会发生抖动的现象)
+ */
 public class SlideContentLayout extends RelativeLayout {
 
     private static final String TAG = "SlideContentLayout";
 
     private MarginLayoutParams marginLayoutParams;
     private boolean hasLoadFlag = false;
-    private int topMargin;
-    private int parentHeight = 1200;
+
+    private RecyclerView recyclerView;
+    private VelocityTracker mVelocityTracker;
+
+    private int contentLayoutHeight;
+    private ContentMode currentMode = ContentMode.MIDDLE_MODE;
+
+    private enum ContentMode{
+        FULL_MODE,
+        MIDDLE_MODE,
+        NONE_MODE
+    }
 
     public SlideContentLayout(Context context) {
         this(context, null);
@@ -36,70 +56,191 @@ public class SlideContentLayout extends RelativeLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         if (changed && !hasLoadFlag) {
+            contentLayoutHeight = b - t;
             Log.i(TAG, "onLayout getLayoutParams l:" + l + ",t:" + t + ",r:" + r + ",b:" + b);
             marginLayoutParams = (MarginLayoutParams) getLayoutParams();
-            Log.i(TAG, "onLayout topMargin:" + marginLayoutParams.topMargin + ",bottomMargin" + marginLayoutParams.bottomMargin
-                    + ",width:" + marginLayoutParams.width + ",height:" + marginLayoutParams.height);
 
             Log.i(TAG, "onLayout getMeasuredWidth:" + getMeasuredWidth() + ",getMeasuredHeight:" + getMeasuredHeight());
             Log.i(TAG, "onLayout getWidth:" + getWidth() + ",getHeight:" + getHeight());
             hasLoadFlag = true;
-
+            recyclerView = (RecyclerView) getChildAt(0);
         }
     }
+
+    float initY1;
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        boolean shouldInterceptFlag = false;
-        topMargin = marginLayoutParams.topMargin;
-        Log.i(TAG, "onInterceptTouchEvent topMargin:" + topMargin);
-        if (topMargin >= 0) {
-            shouldInterceptFlag = true;
+
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, "#### onInterceptTouchEvent  ACTION_DOWN ");
+                initY1 = ev.getRawY();
+                if (null == mVelocityTracker) {
+                    mVelocityTracker = VelocityTracker.obtain();
+                } else {
+                    mVelocityTracker.clear();
+                }
+                mVelocityTracker.addMovement(ev);
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "#### onInterceptTouchEvent  ACTION_MOVE ");
+                float delY1 = ev.getRawY() - initY1;
+                Log.i(TAG, "####onInterceptTouchEvent  ACTION_MOVE getY:" + getY());
+                Log.i(TAG, "####onInterceptTouchEvent  ACTION_MOVE shouldIntercept():" + shouldIntercept());
+                //手指下滑
+                //进行事件拦截判断
+                if (delY1 > 0 && Math.abs(delY1) > 2){
+                    if (shouldIntercept() && (getY() == 0) || getY() > 0){
+                        initY1 = ev.getRawY();
+                        Log.i(TAG, "####onInterceptTouchEvent 11111111 return true... ");
+                        //进行事件拦截
+                        return true;
+                    }
+                }
+
+                //手指上滑
+                //进行事件拦截判断
+                if (delY1 < 0 && Math.abs(delY1) > 2){
+                    if ( getY() > 0){
+                        initY1 = ev.getRawY();
+                        Log.i(TAG, "####onInterceptTouchEvent 222222222 return true... ");
+                        //进行事件拦截
+                        return true;
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG, "#### onInterceptTouchEvent  ACTION_UP ");
+
+
+                break;
         }
-        return shouldInterceptFlag;
+
+        return super.onInterceptTouchEvent(ev);
     }
 
-    float initX;
-    float initY;
+    /**
+     * recyclerview 是否拉到顶端
+     * @return
+     */
+    private boolean shouldIntercept(){
+        View firstChild = recyclerView.getChildAt(0);
+        boolean shouldIntercept;
+        LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        int firstVisiblePosition = linearLayoutManager.findFirstVisibleItemPosition();
+        if (firstVisiblePosition == 0 && firstChild.getTop() == 0){
+            shouldIntercept = true;
+        } else {
+            shouldIntercept = false;
+        }
+        return shouldIntercept;
+    }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                initY = event.getY();
-                Log.i(TAG, "onTouchEvent  ACTION_DOWN topMargin:" + topMargin);
-                if (marginLayoutParams.topMargin >= 0) {
-                    return true;
-                }
 
                 break;
             case MotionEvent.ACTION_MOVE:
-                Log.i(TAG, "onTouchEvent  ACTION_MOVE topMargin:" + topMargin);
-                Log.i(TAG, "onTouchEvent  ACTION_MOVE parentHeight:" + parentHeight);
+                Log.i(TAG, "####onTouchEvent  ACTION_MOVE shouldIntercept():" + shouldIntercept());
 
-                marginLayoutParams = (MarginLayoutParams) getLayoutParams();
+                Log.i(TAG, "####onTouchEvent  ACTION_MOVE getY:" + getY());
+                mVelocityTracker.addMovement(event);
+                mVelocityTracker.computeCurrentVelocity(1000);
 
-                float delY = event.getY() - initY;
-                if (delY > 0){
-                    if (Math.abs(delY) > 2 && (marginLayoutParams.height > 20)){
-                        marginLayoutParams.height = marginLayoutParams.height - (int) delY;
-                        initX = event.getY();
-                        setLayoutParams(marginLayoutParams);
+                float delY = event.getRawY() - initY1;
+                Log.i(TAG, "####onTouchEvent  ACTION_MOVE delY:" + delY);
+
+                //手指下滑
+                //delY的判断，消除上下抖动
+                if (delY > 0 && Math.abs(delY) > 2){
+                    if (getY() >= 0){
+                        initY1 = event.getRawY();
+                        setY(getY() + delY);
+                        Log.i(TAG, "####onTouchEvent  ACTION_MOVE down requestLayout:");
+
+                        requestLayout();
+                        //进行事件消耗
+                        return true;
                     }
                 }
-                if (delY < 0){
-                    if (Math.abs(delY) > 2 && marginLayoutParams.height < parentHeight){
-                        marginLayoutParams.height = marginLayoutParams.height - (int) delY;
-                        initX = event.getY();
-                        setLayoutParams(marginLayoutParams);
+                //手指上滑
+                //delY的判断，消除上下抖动
+                if (delY < 0 && Math.abs(delY) > 2){
+                    if ( getY() > 0){
+                        initY1 = event.getRawY();
+                        if ((getY() - Math.abs(delY)) < 0){
+                            setY(0);
+                        } else {
+                            setY(getY() - Math.abs(delY));
+                        }
+                        Log.i(TAG, "####onTouchEvent  ACTION_MOVE up requestLayout:");
+                        requestLayout();
+                        //进行事件消耗
+                        return true;
                     }
                 }
-
-                Log.i(TAG, "onTouchEvent  ACTION_MOVE height:" + marginLayoutParams.height);
-
-
                 break;
+            case MotionEvent.ACTION_UP:
+                processFinishEvent();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+
         }
         return super.onTouchEvent(event);
+    }
+
+    private void processFinishEvent(){
+        autoMoveAnimator(1000);
+    }
+
+    private void autoMoveAnimator(int duration){
+        float start = 0;
+        float end = 0;
+        if (getY()> 0 && getY() < contentLayoutHeight / 4){
+            start = 0;
+            end = - getY();
+            Log.i(TAG, "#### autoMoveAnimator 111111111111111 end:" + end );
+
+        }
+        if (getY() > contentLayoutHeight/4 && getY() < contentLayoutHeight * 3 / 4){
+            start = 0;
+            end = contentLayoutHeight / 2 - getY();
+            Log.i(TAG, "#### autoMoveAnimator 2222222222222222 end:" + end);
+
+        }
+        if (getY() > contentLayoutHeight * 3 / 4 && getY() < contentLayoutHeight){
+            start = 0;
+            end = contentLayoutHeight - getY() + -60;
+            Log.i(TAG, "#### autoMoveAnimator 33333333333333333333 end:" + end );
+
+        }
+
+//        if (mode == ContentMode.FULL_MODE){
+//            end = 0;
+//        } else if (mode == ContentMode.MIDDLE_MODE){
+//            end = contentLayoutHeight / 2;
+//        } else if (mode == ContentMode.NONE_MODE){
+//            end = contentLayoutHeight - 20;
+//        }
+        final float currentPosition = getY();
+        ValueAnimator animator = ValueAnimator.ofFloat(start, end);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float result = (float) animation.getAnimatedValue();
+                Log.i(TAG, "#### onAnimationUpdate  onAnimationUpdate:" + result);
+                setY(currentPosition + result);
+                requestLayout();
+            }
+        });
+        animator.setDuration(duration);
+        animator.start();
     }
 }
